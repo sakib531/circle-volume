2d934312fee0aa3defd6ca06fbc0f7ac74c8bf3c
sakib531 <mdsakibahammad531@gmail.com>
2024-02-19 23:13:08 +0600

    first commit

---
+++ circle-volume.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main (){
+    float a;
+    printf ("enter the value of 'a' which is the radius of a sphere:");
+    scanf("%f",&a);
+    printf ("the volume of the sphere's circle is :");
+    printf ("%f" ,1.33*3.1426*a*a*a);
+    return 0;
+}
